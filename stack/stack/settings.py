6711c721e7f690896a3a05d085f09639ff5e5bbc
cjhird <charlie.hird@googlemail.com>
2022-12-12 23:25:47 +0000

    scraped and inserted into MongoDB

--- stack/stack/settings.py
+++ stack/stack/settings.py
@@ -62,9 +62,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'stack.pipelines.StackPipeline': 300,
-#}
+ITEM_PIPELINES = {'stack.pipelines.MongoDBPipeline': 100, }
+
+MONGO_URI = 'mongodb://localhost:27017'
+MONGO_DATABASE = "stackoverflow"
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
